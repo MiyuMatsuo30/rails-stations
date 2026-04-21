ed53c4e9ca2ad9514de9653431ca4928f2a293e2
MiyuMatsuo30 <matsuo30m@gmail.com>
2026-04-21 11:27:36 +0900

    14

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
     add_flash_types :secondary, :success, :danger, :warning, :info, :light, :dark
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
